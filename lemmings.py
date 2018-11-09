2570999ba985a274dcd3a49306b3bfc18e6da01a
Tymoteusz Makowski <38053499+tmakowski@users.noreply.github.com>
2018-11-09 01:23:12 +0100

    Breaking the ice

---
+++ lemmings.py
@@ -0,0 +1,17 @@
+"""
+File containing classes for all lemming types. That is:
+    - lemming
+
+"""
+
+
+class Lemming:
+    """
+    This class is going to represent each lemming.
+    """
+
+
+class LemmingStopper (Lemming):
+    """
+    This extended class is going to represent the lemming with a stopper function.
+    """
