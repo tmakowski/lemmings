0b13c7285b2b1ba2f68209e5d05796c0abc1b1f5
tmakowski <tymek.makowski@gmail.com>
2018-11-14 13:17:16 +0100

    objects list to dict

--- level_generator.py
+++ level_generator.py
@@ -1,14 +1,16 @@
 """
 The purpose of this module is translating text visualization of the map to the proper objects.
 """
-from classes.objects import *
-from global_variables import *
-
+from classes.objects import Floor, Wall
+from global_variables import BLOCK_SIZE, OBJECTS_NAMES
 
 DICT_CODE_TO_OBJ = {"W": Wall, "F": Floor}
 
 
 def generate_level(level_layout):
+    """
+    This function creates objects used in the level based in visualization of the level layout.
+    """
     # Variables initialization
     objects = []
     offset_y = 0
@@ -28,4 +30,14 @@ def generate_level(level_layout):
 
         # Moving to the next line
         offset_y += 1
-    return objects
+    return dict_sort_objects(objects)
+
+
+def dict_sort_objects(objects):
+    """
+    This function generates dictionary for 'object name: list of objects with class' name == object name'.
+    """
+    dict_objects = {}
+    for obj_name in OBJECTS_NAMES:
+        dict_objects[obj_name] = [obj for obj in objects if obj.__class__.__name__ == obj_name]
+    return dict_objects
