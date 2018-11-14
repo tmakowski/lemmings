8bedea825c0d92c94cbb322f7163d4b660e13f53
tmakowski <tymek.makowski@gmail.com>
2018-11-14 14:18:09 +0100

    import/global variables cleanup

--- classes/objects.py
+++ classes/objects.py
@@ -1,18 +1,25 @@
 import pygame
-from global_variables import *
+from global_variables import BLOCK_SIZE,\
+    OBJECT_GRAPHICS_FLOOR, OBJECT_GRAPHICS_WALL
 
 
 class Floor:
-    def __init__(self, position_x, position_y, length_x=None, length_y=None, img="graphics/floor.png"):
+    """
+    Parent class for all objects' classes.
+    """
+    def __init__(self, position_x, position_y, length_x=None, length_y=None, img=OBJECT_GRAPHICS_FLOOR):
+        """
+        Creates new floor at position (x, y) that can be stretched by providing length_x/_y factors.
+        """
         # Default values for scaling
         if length_x is None:
             length_x = 1
         if length_y is None:
             length_y = 1
 
-        # Scaling of the image
+        # Scaling and loading the image
         self.image = pygame.transform.scale(
-                        pygame.image.load(img).convert(),
+                        pygame.image.load(img),
                         (BLOCK_SIZE * length_x, BLOCK_SIZE * length_y))
 
         # Creating the hitbox
@@ -20,5 +27,11 @@ class Floor:
 
 
 class Wall (Floor):
+    """
+    Subclass representing walls.
+    """
     def __init__(self, position_x, position_y, length_x=None, length_y=None):
-        super(self.__class__, self).__init__(position_x, position_y, length_x, length_y, img="graphics/wall.png")
+        """
+        Calls the constructor from parent class (Floor) with image representing a wall.
+        """
+        super(self.__class__, self).__init__(position_x, position_y, length_x, length_y, img=OBJECT_GRAPHICS_WALL)
--- pygame_test.py
+++ pygame_test.py
@@ -1,78 +1,77 @@
-from classes import *
-from global_variables import *
+import pygame
 import sys
 import time
-# import pygame
 
-# Initialization
-pygame.init()
-
-# Defining screen
-screen = pygame.display.set_mode(LEVEL_SIZE)
-
-# Testing only: initializing objects
-lemmings = [Lemming(20, 20), Lemming(150, 84)]
-#walls = [Wall(250, 0, length_y=5), Wall(250+BLOCK_SIZE*1+1, 0), Wall(250+BLOCK_SIZE*1, BLOCK_SIZE*1),
-#         Wall(0, 0), Wall(BLOCK_SIZE, BLOCK_SIZE), Wall(2*BLOCK_SIZE, 0)]
-#floors = [Floor(100, 300, 7), Floor(0, 200), Floor(0, 100, 5)]
+from classes.lemmings import Lemming
+from level_generator import generate_level
+from global_variables import BLOCK_SIZE,\
+    LEVEL_SIZE, LEVEL_DEATH_FRAMES, LEVEL_FRAME_TIME
 
 
 level = [
     "WWWWWWWWWWWWWWWWWWWW",
+    "W      W           W",
+    "W      W           W",
+    "WFFFFF W           W",
+    "W      W           W",
+    "WF FFFFF           W",
     "W                  W",
     "W                  W",
-    "W                  W",
-    "W                  W",
-    "W                  W",
-    "W                  W",
-    "W                  W",
-    "W                  W",
+    "WFFFFFF            W",
     "W                  W",
     "W                  W",
     "W                  W",
     "W                  W",
     "W                  W",
     "WFFFFFFFFFFFFFFFFFFW"]
-#
-from level_generator import *
-objects = generate_level(level)
-floors = [obj for obj in objects if type(obj) == Floor]
-walls = [obj for obj in objects if type(obj) == Wall]
-#
+
+
+# Level startup
+pygame.init()
+screen = pygame.display.set_mode(LEVEL_SIZE)    # setting screen of the globally set size
+dict_objects = generate_level(level)            # generating objects based on the level visualization
+
+# Testing only: initializing objects
+lemmings = [Lemming(BLOCK_SIZE, BLOCK_SIZE)]
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # Lemming exit check
-
-    # Moving each lemming
+# Performing actions for each lemming
     for lem in lemmings:
-        # Check if lemming is dead
+
+        # Check if the lemming is dead
         if lem.dead > 0:
-            # If it is dead then we display it's dead version for set number of frames and remove it afterwards
-            if lem.dead > LEMMING_DEATH_FRAMES:
+
+            # Deleting the lemming after it's dead version has been displayed long enough
+            if lem.dead > LEVEL_DEATH_FRAMES:
                 lemmings.remove(lem)
+
+            # Increasing the dead-lemming frame counter
             lem.dead += 1
             continue
 
-        lem.collision_walls(walls)
-        lem.collision_floors(floors)
+        # Colliding the lemming with each type of objects
+        lem.collision(dict_objects)
 
+        # Moving the lemming
         lem.move()
+
 
 # Drawing the frame
     # Filling background
     screen.fill((0, 0, 0))
 
     # Drawing all objects
-    for obj in objects+lemmings:
+    for obj in lemmings+dict_objects.values():
         screen.blit(obj.image, obj.rect)
 
     # Changing display to show drawn objects
     pygame.display.flip()
 
+
 # System stuff
     # Setting custom pause between frames
-    time.sleep(FRAME_TIME)
+    time.sleep(LEVEL_FRAME_TIME)
